168f58d7a7043e5f59207bdb6503a71077d6ac49
thasnithajudheen <thasnithajudheen0@gmail.com>
2021-05-06 20:12:12 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { FilterComponent } from './filter/filter.component';
+import { CartComponent } from './cart/cart.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductlistComponent,
+    FilterComponent,
+    CartComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
